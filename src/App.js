81fdc85ff6b9ec8da287a299522b3fad18b6675c
KC Kern <kc@kckern.com>
2021-12-17 15:27:52 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,20 +5,28 @@ import SendBird from "sendbird";
 function App() {
 
   const [members, setMembers] = useState([]);
+  const [fetching, setFetching] = useState(false);
 
-  var sb = new SendBird({ appId: "386311F5-8923-4F4D-8BE1-C3E95C2AD963", localCacheEnabled: true });
-  sb.connect("martinharris", "728a2c97f4900b0e869a7b7ab7cf3e727252abc1", function (user, error) {
-    if (error) {
-      // Handle error.
-    }
-
-    sb.GroupChannel.getChannel("e7a4763dec356ef015782e5281cb95cf", function (groupChannel, error) {
+  if(!fetching && members.length===0)
+  {
+    setFetching(true);
+    var sb = new SendBird({ appId: "386311F5-8923-4F4D-8BE1-C3E95C2AD963", localCacheEnabled: true });
+    sb.connect("martinharris", "728a2c97f4900b0e869a7b7ab7cf3e727252abc1", function (user, error) {
       if (error) {
         // Handle error.
       }
-      setMembers(groupChannel.members)
+  
+      sb.GroupChannel.getChannel("e7a4763dec356ef015782e5281cb95cf", function (groupChannel, error) {
+        if (error) {
+          // Handle error.
+        }
+        setMembers(groupChannel.members)
+        setFetching(false);
+      });
     });
-  });
+  
+
+  }
 
   return (
     <div className="App">
