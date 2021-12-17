12bd72e5945a9d22e6afe34c6d3128c08f081b71
KC Kern <kc@kckern.com>
2021-12-17 14:34:25 +0900

    Rebase

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import './App.css';
+import SendBird from "sendbird";
+
+function App() {
+
+  const [members, setMembers] = useState([]);
+
+  var sb = new SendBird({ appId: "386311F5-8923-4F4D-8BE1-C3E95C2AD963", localCacheEnabled: true });
+  sb.connect("tytus", "da8107ba9a97a47cbba61e29085d1a160d44e46b", function (user, error) {
+    if (error) {
+      // Handle error.
+    }
+
+    sb.GroupChannel.getChannel("e7a4763dec356ef015782e5281cb95cf", function (groupChannel, error) {
+      if (error) {
+        // Handle error.
+      }
+      setMembers(groupChannel.members)
+    });
+  });
+
+  return (
+    <div className="App">
+      <h1>Test IMGS</h1>
+      <div className='imgs'>
+      {members.map(member => <div><img src={member.profileUrl} /></div>)}
+      </div>
+    </div>
+  );
+}
+
+export default App;
